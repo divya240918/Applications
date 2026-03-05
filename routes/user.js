1512c75b850c5ad154dbc77bf1a38405f3e99af3
Divya Das <dasdivya589@gmail.com>
2026-03-05 20:15:39 +0530

    Delete routes directory

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const express  = require('express');
-const router = express.Router();
-
-router.post('/signup', handleUserSignUp);
-
-router.post('/login', handleUserLogin);
-
-module.exports = router;
